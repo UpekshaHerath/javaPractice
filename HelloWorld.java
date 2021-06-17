a524f343c296af0768c2b459bde78ac75c2082cf
Upeksha0 <65499669+Upeksha0@users.noreply.github.com>
2021-06-17 00:11:18 -0700

    added a for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello Java!!");
+		for(int i = 0 ; i < 5; i++){
+			System.out.println("Gitnub practice!!");
+		
+		}
 
 }
 
