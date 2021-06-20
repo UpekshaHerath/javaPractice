cdbc4f86040bb87bcc2c4ccb341c818c85810b23
UpekshaHerath <65499669+UpekshaHerath@users.noreply.github.com>
2021-06-19 22:05:57 -0700

    added a comment

--- Practice2.java
+++ Practice2.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 public class Practice2{
 
 	public static void main(String[] args){
-	
+		//This is just a for loop to print "this is git" 10 times.....
 		for(int i = 0; i < 10; i++){
 			
 			out.println("this is git");
