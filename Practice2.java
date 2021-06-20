d70d98b9d7359eade02e935cce053987e6c55108
Upe_ksha_Dilshan123 <upekshadilshan111@gmail.com>
2021-06-20 10:10:31 -0700

    added two new java files just to practice git

---
+++ Practice2.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+public class Practice2{
+
+	public static void main(String[] args){
+	
+		for(int i = 0; i < 10; i++){
+			
+			out.println("this is git");
+		
+		}
+
+
+	}
+
+
+}
